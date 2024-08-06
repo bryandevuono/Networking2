1fbc8da363851dd80ccbe00cf1d602fa802ec77e
JulinaMay <julinamay_@Hotmail.com>
2024-08-06 15:56:54 +0200

    ALleen Hello

--- client/Program.cs
+++ client/Program.cs
@@ -20,25 +20,66 @@ class ClientUDP
 
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
+
+    private UdpClient client;
+    private IPEndPoint serverEndpoint;
+
+    public ClientUDP()
+    {
+        client = new UdpClient();
+        serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000); // Verander het IP-adres en de poort indien nodig
+    }
     public void start()
     {
-
+        try
+        {
+            SendHello();
+            // ReceiveWelcome();
+            // SendThreshold(10);
+            // ReceiveAck();
+            // RequestData("example.txt");
+            // ReceiveData();
+            // SendAck();
+            // ReceiveEnd();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            client.Close();
+        }
     }
     //TODO: create all needed objects for your sockets 
 
     //TODO: [Send Hello message]
+    private void SendHello()
+    {
+        string message = "HELLO";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        client.Send(data, data.Length, serverEndpoint);
+        Console.WriteLine("Sent: HELLO");
+    }
 
     //TODO: [Receive Welcome]
+    private void ReceiveWelcome()
+    {
+        var response = client.Receive(ref serverEndpoint);
+        string message = Encoding.UTF8.GetString(response);
+        Console.WriteLine($"Received: {message}");
+    }
+
 
     //TODO: [Send Data(threshold)]
-    
+
     //TODO: [Receive ACK]
 
     //TODO: [Send RequestData]
 
-        //TODO: [Receive Data]
+    //TODO: [Receive Data]
 
-        //TODO: [Send ACK]
+    //TODO: [Send ACK]
 
     //TODO: [Receive END]
 
--- server/Program.cs
+++ server/Program.cs
@@ -23,9 +23,32 @@ class ServerUDP
 
     //TODO: implement all necessary logic to create sockets and handle incoming messages
     // Do not put all the logic into one method. Create multiple methods to handle different tasks.
+    private UdpClient server;
+    private IPEndPoint clientEndpoint;
+
+    public ServerUDP()
+    {
+        server = new UdpClient(9000); // Luisteren op poort 9000
+        clientEndpoint = new IPEndPoint(IPAddress.Any, 0);
+    }
+
     public void start()
     {
-
+        try
+        {
+            while (true)
+            {
+                ReceiveMessage();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            server.Close();
+        }
     }
 
     //TODO: create all needed objects for your sockets 
@@ -34,8 +57,38 @@ class ServerUDP
     // you can call a dedicated method to handle each received type of messages
 
     //TODO: [Receive Hello]
+    private void ReceiveMessage()
+    {
+        try
+        {
+            var data = server.Receive(ref clientEndpoint);
+            string message = Encoding.UTF8.GetString(data);
+            Console.WriteLine($"Received: {message}");
+
+            if (message == "HELLO")
+            {
+                SendWelcome();
+            }
+            else
+            {
+                Console.WriteLine("Unexpected message received");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while receiving message: {ex.Message}");
+        }
+    }
 
     //TODO: [Send Welcome]
+    private void SendWelcome()
+    {
+        string message = "WELCOME";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        server.Send(data, data.Length, clientEndpoint);
+        Console.WriteLine("Sent: WELCOME");
+    }
+
 
     //TODO: [Receive Data]
 
@@ -45,14 +98,14 @@ class ServerUDP
 
     //TODO: [Send Data]
 
-        //TODO: [Implement your slow-start algorithm considering the threshold] 
+    //TODO: [Implement your slow-start algorithm considering the threshold] 
 
     //TODO: [End sending data to client]
 
     //TODO: [Handle Errors]
 
     //TODO: [Send End]
-    
+
     //TODO: create all needed methods to handle incoming messages
 
 
